fe380c8f02f35534bb830c5b2d582f9da02b2788
pablo <hamburguesasinsalsa@gmail.com>
2021-10-08 22:51:02 -0500

    storage

--- components/database/Add.js
+++ components/database/Add.js
@@ -1,44 +1,53 @@
-import { Formik, Field, Form } from 'formik';
-import { dataBase } from './config';
+import { Formik, Field, Form, set } from 'formik';
+import { dataBase, storage } from './config';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useState } from 'react';
 
 const Add = () => {
 
-    const { user } = useAuth0();
+  const { user } = useAuth0();
+  const [link, setLink] = useState('');
 
-    const sent = values => {
-        dataBase.collection('recipes')
-        .add({
-            name: values.name,
-            description: values.description,
-            type: values.type,
-            author: user.name,
-            idAuthor: user.email,
-            image: values.image,
-            date: new Date(),
-        })
-        .then( () => {
-          console.log('New recipe added');
-          window.location.reload();
-        });
-    };
+  const fileStore = async e => {
+    const file = e.target.files[0];
+    const storageRef = storage.ref();
+    const path = storageRef.child(file.name);
+    await path.put(file);
+    const linkUrl = await path.getDownloadURL();
+    setLink(linkUrl);
+  };
 
-    return (
-        <Formik initialValues={{ name: '', type: '', description: '', image: '' }} onSubmit={sent} >
-          <Form>
-            <Field name="name" type="text" required />
-            <Field name="description" as="textarea" required />
-            <Field name="image" type="file" required />
-            <Field name="type" as="select" required >
-              <option value="" >Elige el tipo de tu receta</option>
-              <option value="Pasteleria" >Pasteleria</option>
-              <option value="Vegetariano" >Vegetariano</option>
-            </Field>
-            <button type="submit" >Submit</button>
-          </Form>
-        </Formik>
-    );
+  const sent = values => {
+    dataBase.collection('recipes')
+    .add({
+        name: values.name,
+        description: values.description,
+        type: values.type,
+        author: user.name,
+        idAuthor: user.email,
+        image: link,
+        date: new Date(),
+    })
+    .then( () => {
+      //window.location.reload();
+    });
+  };
 
+  return (
+    <Formik initialValues={{ name: '', type: '', description: '' }} onSubmit={sent} >
+      <Form>
+        <Field name="name" type="text" required />
+        <Field name="description" as="textarea" required />
+        <input name="file" type="file" onChange={fileStore} required />
+        <Field name="type" as="select" required >
+          <option value="" >Elige el tipo de tu receta</option>
+          <option value="Pasteleria" >Pasteleria</option>
+          <option value="Vegetariano" >Vegetariano</option>
+        </Field>
+        <button type="submit" >Submit</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default Add;
--- components/database/Get.js
+++ components/database/Get.js
@@ -23,6 +23,7 @@ const Get = props => {
                 {recipes.map(doc => {
                     return (
                         <div key={recipes.indexOf(doc)} >
+                            <img src={doc.image} />
                             <h1>{doc.name}</h1>
                             <h3>{doc.type}</h3>  
                             <h5>{doc.author}</h5>
